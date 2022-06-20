d935eb1d7b7812b2cfe758fb41e0a3f1c2b0d4e0
Jawher B <jawher_bouhouch@yahoo.com>
2022-06-20 09:34:09 +0200

    add e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,23 @@
 
 
-describe('empty spec', () => {
+describe('landing page spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3002/')
   })
 
-  it('', () => {
+  it('render results upon search', () => {
     cy.visit('http://localhost:3002/')
+    //ACT
+    cy.get('input').type('butterjack{enter}')
+    //ASSERT
+    cy.get('.repos').should('be.visible')
+  })
+
+  it('result upon non-valid username', () => {
+    cy.visit('http://localhost:3002/')
+    //ACT
+    cy.get('input').type('non-valid-username{enter}')
+    //ASSERT
+    cy.contains('No such user')
   })
 })
